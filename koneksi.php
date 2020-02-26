6a4fbcb9ff8d135e61357e2a710eb3aa57f80418
csynoers <3s0c9m7@gmail.com>
2020-02-26 20:52:40 +0700

    setting koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,9 @@
 <?php
-    $koneksi = new mysqli("localhost","root","","danang");
-?>
+    ini_set('date.timezone', 'Asia/Jakarta');
+    define("HOST","localhost");
+    define("USERNAME","locdowns_store");
+    define("PASSWORD","3.s.0.c.9.m.7");
+    define("DATABASE","locdowns_store");
+
+    /* set config connection procedural */
+    $koneksi = new mysqli(HOST,USERNAME,PASSWORD,DATABASE);
