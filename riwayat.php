a1799fd61ebc3c617811633a7bb6d303e540e3f0
csynoers <3s0c9m7@gmail.com>
2020-03-01 20:17:28 +0700

    riwayat belanja

--- riwayat.php
+++ riwayat.php
@@ -27,27 +27,29 @@ if (!isset($_SESSION["pelanggan"]) OR empty($_SESSION["pelanggan"]))
 				// mendapatkan id_pelanggan yang login dari session
 				$id_pelanggan = $_SESSION["pelanggan"]["id_pelanggan"];
 
-				$ambil = $koneksi->query("SELECT * FROM pembelian WHERE id_pelanggan='$id_pelanggan'");
-				while ($pecah = $ambil->fetch_assoc()) {
+				$db->query = ("SELECT * FROM pembelian WHERE id_pelanggan='$id_pelanggan'");
+				foreach ($db->query() as $key => $value) {
+					// echo json_encode($value);
+					$value->status_pembayaran_mod = strtoupper(str_replace('_',' ',$value->status_pembayaran));
 					?>
 				<tr>
 					<td><?php echo $nomor; ?></td>
-					<td><?php echo $pecah["tanggal_pembelian"] ?></td>
+					<td><?= $value->tanggal_pembelian ?></td>
 					<td>
-						<?php echo $pecah["status_pembelian"] ?>
+						<?= $value->status_pembayaran_mod ?>
 							<br>
 							<?php if (!empty($pecah['resi_pengiriman'])): ?>
 								Resi : <?php echo $pecah['resi_pengiriman']; ?>
 							<?php endif ?>
 						</td>
-					<td>Rp. <?php echo number_format($pecah["total_pembelian"]) ?></td>
+					<td>Rp. <?php echo number_format($value->total_pembelian) ?></td>
 					<td>
-						<a href="index.php?page=nota&id=<?php echo $pecah["id_pembelian"] ?>" class="btn btn-info">Nota</a>
-						<?php if ($pecah['status_pembelian']=="pending"): ?>
-						<a href="index.php?page=pembayaran&id=<?php echo $pecah["id_pembelian"];?>" class="btn btn-success">Input Pembayaran</a>
-						<?php else: ?>
-							<a href="index.php?page=lihat_pembayaran&id=<?php echo $pecah["id_pembelian"]; ?>" class="btn btn-warning">Lihat Pembayaran</a>
-					<?php endif ?>
+						<a href="index.php?page=nota&id=<?php echo $value->id_pembelian ?>" class="btn btn-info">Nota</a>
+						<?php
+							if ( ($value->status_pembayaran=='belum_dibayar') && empty($value->metode_pembayaran) ) {
+								echo "<a href='{$value->invoice_url}' class='btn btn-success'>Bayar Sekarang</a>";
+							}
+						?>
 					</td>
 				</tr>
 				<?php $nomor++; ?>
