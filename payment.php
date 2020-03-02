c354b378fc8bf6a1438d62fd4a095df272abeb47
csynoers <3s0c9m7@gmail.com>
2020-03-02 10:03:34 +0700

    debug >> payment >> off

--- payment.php
+++ payment.php
@@ -17,18 +17,19 @@
                 # get data id for WHERE condition to change field tabel in your database
                 $this->data['id']               = $this->data['responseDecode']->id;
                 
-                // $this->db->query = "INSERT INTO tes(des) VALUES ('{$this->data['response']}') ";
+                // $this->db->query = "INSERT INTO tes(des) VALUES ('{$this->data['responseDecode']->payment_method} ({$this->data['responseDecode']->bank_code})') ";
                 // $this->db->query_exec();
+                // die();
                 switch ( $this->data['responseDecode']->status ) {
                     case 'PAID':
                         # code controller paid payment here...
-                        $this->db->query = "UPDATE `pembelian` SET `status_pembayaran`='sudah_dibayar',`status_pesanan`='sedang_dikemas' WHERE `external_id`='{$result->id}' ";
+                        $this->db->query = "UPDATE `pembelian` SET `status_pembayaran`='sudah_dibayar',`metode_pembayaran`='{$this->data['responseDecode']->payment_method} ({$this->data['responseDecode']->bank_code})',`status_pesanan`='sedang_dikemas' WHERE `external_id`='{$this->data['id']}' ";
                         $this->db->query_exec();
                         break;
                     
                     case 'EXPIRED':
                         # code controller expired payment here...
-                        $this->db->query = "UPDATE `pembelian` SET `status_pembayaran`='kadaluarsa' WHERE `external_id`='{$result->id}' ";
+                        $this->db->query = "UPDATE `pembelian` SET `status_pembayaran`='kadaluarsa' WHERE `external_id`='{$this->data['id']}' ";
                         $this->db->query_exec();
                         break;
                     
