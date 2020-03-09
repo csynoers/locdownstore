1795e39ff19e328e871afb800cabf1ca9676a2c6
csynoers <3s0c9m7@gmail.com>
2020-03-09 11:28:56 +0700

    fix >> halaman detail produk >> stok produk & jika produk kosong(keterangan stok habis & tombol beli dihilangkan)

--- detail.php
+++ detail.php
@@ -16,14 +16,14 @@
 			<h2><?php echo $detail["nama_produk"] ?></h2>
 			<h4>Rp. <?php echo number_format($detail["harga_produk"]); ?></h4>
 
-			<h5>Stok : <?php echo $detail['stok_produk'] ?></h5>
+			<h5>Stok : <?= ($detail['stok_produk'] > 0 ? $detail['stok_produk'] : '<span class="text-danger">-Maaf stok habis-</span>' ) ?></h5>
 
-			<form method="post">
+			<form method="post" class="<?= ($detail['stok_produk'] > 0 ? NULL : 'hidden' ) ?>">
 				<div class="form-group">
-					<div class="input-group">
-					<input type="number" min="1" class="form-control" name="jumlah" max="<?php echo $detail['stok_produk'] ?>">
-					<div class="input-group-btn">
-						<button class="btn btn-primary" name="beli">Beli</button>
+					<div class="input-group col-md-3">
+						<input value="1" type="number" min="1" class="form-control" name="jumlah" max="<?php echo $detail['stok_produk'] ?>" required="">
+						<div class="input-group-btn">
+							<button class="btn btn-primary" name="beli">Beli</button>
 						</div>
 					</div>
 				</div>
@@ -39,11 +39,12 @@
 				$_SESSION["keranjang"][$id_produk] = $jumlah;
 
 				echo "<script>alert('produk telah masuk ke keranjang belanja');</script>";
-				echo "<script>location='keranjang.php';</script>";
+				echo "<script>location='index.php?page=keranjang';</script>";
 			}
 			?>
-
-			<p><?php echo $detail["deskripsi_produk"]; ?></p>
+			<hr>
+			<h4>Deskripsi</h4>
+			<p class="text-justify"><?php echo $detail["deskripsi_produk"]; ?></p>
 		</div>
 	</div>
 </section>
