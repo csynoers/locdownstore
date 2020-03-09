24d0ee4adf58e3dc926ff253f06a0da88a757aef
csynoers <3s0c9m7@gmail.com>
2020-03-09 22:54:14 +0700

    admin::logout

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@
 if (!isset($_SESSION['admin']))
 {
     // echo "<script>alert('Anda Harus Login');</script>";
-    echo "<script>location='login.php;'</script>";
+    echo "<script>location='login.php'</script>";
     // header('location:login.php');
     // exit();
 }
