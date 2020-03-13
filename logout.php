c7cf5b648fa8aac440d15e08daff768cb9b5b037
csynoers <3s0c9m7@gmail.com>
2020-03-14 01:45:34 +0700

    logout

--- logout.php
+++ logout.php
@@ -3,8 +3,6 @@ session_start();
 
 // menghancurkan $_SESSION["pelanggan"]
 session_destroy();
-
-die();
 echo "<script>alert('anda telah logout');</script>";
 echo "<script>location='index.php';</script>";
 
