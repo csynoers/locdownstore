6376ff7e764a99d4d9f79b496ad59da661f9b11d
csynoers <3s0c9m7@gmail.com>
2020-03-14 01:34:38 +0700

    create invoice

--- create_invoice.php
+++ create_invoice.php
@@ -6,7 +6,7 @@
     $invoice = $xendit->createInvoice($_GET['id'], $_GET['amount'], $_GET['email'], $_GET['keterangan']);
     if ( $invoice['id'] ) {
         $query = [];
-        $query['query_insert_pembelian'] = ("INSERT INTO `pembelian`(`id_pembelian`, `id_pelanggan`, `tanggal_pembelian`, `total_pembelian`, `external_id`, `invoice_url`, `status_pembayaran`) VALUES ('{$_SESSION['order_id']}','{$_SESSION['pelanggan']['id_pelanggan']}','".date('Y-m-d H:i:s')."','{$_GET['amount']}','{$invoice['id']}','{$invoice['invoice_url']}','belum_dibayar')");
+        $query['query_insert_pembelian'] = ("INSERT INTO `pembelian`(`id_pembelian`, `id_pelanggan`, `tanggal_pembelian`, `total_pembelian`, `external_id`, `invoice_url`, `status_pembayaran`) VALUES ('{$_SESSION['order_id']}','{$_SESSION['pelanggan']->id_pelanggan}','".date('Y-m-d H:i:s')."','{$_GET['amount']}','{$invoice['id']}','{$invoice['invoice_url']}','belum_dibayar')");
         foreach ($_SESSION["keranjang"] as $id_produk => $jumlah) {
             // $db->query = "SELECT * FROM produk WHERE id_produk='{$id_produk}'";
             // $row = $db->query();
