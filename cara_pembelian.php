5ab00f1f5f1188034b3873f071c1a4b4fb06cc23
csynoers <3s0c9m7@gmail.com>
2020-04-17 06:36:20 +0700

    update cara pembelian

--- cara_pembelian.php
+++ cara_pembelian.php
@@ -9,7 +9,12 @@
 					<p>
 						1. Silahkan pilih produk yang akan anda beli.<br>
 						2. Klik tombol beli atau klik tombol detail untuk mengetahui detail produk yang akan anda beli.<br>
-						3. Silahkan klik tombol beli dan otomatis anda akan diarahkan ke karanjang belanja.
+						3. Silahkan klik tombol beli dan otomatis anda akan diarahkan ke keranjang belanja.<br>
+						4. Jika anda belum mempunyai akun ,maka otomatis anda diarahkan ke menu daftar untuk mendaftar sebagai member terlebih dahulu.<br>
+						5. Setelah mempunyai akun member anda disuruh untuk input email dan password anda untuk masuk ke menu keranjang belanja.<br>
+						6. Jika sudah selesai belanja silahkan pilih ongkos kirim dan selanjutnya klik tombol checkout.<br>
+						7. Setelah klik tombol checkout maka anda akan diarahkan ke menu riwayat belanja dan disuruh untuk melakukan pembayaran.<br>
+						8. Setelah melakukan pembayaran maka status pembayaran anda yang sebelumnya belom dibayar akan menjadi sudah dibayar dan menunggu konfirmasi dari admin.<br>
 					</p>
 				</div>
 			</div>
